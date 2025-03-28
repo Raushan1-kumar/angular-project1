b92ebb33ef3762ed266fc0478799b16874b53abb
raushan <raushansahu147@gmail.com>
2025-03-28 10:16:43 +0530

    add directive functionality

--- src/app/add-numbers/add-numbers.component.ts
+++ src/app/add-numbers/add-numbers.component.ts
@@ -8,5 +8,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-numbers.component.css'
 })
 export class AddNumbersComponent {
-
+    num1: number = 0;
+    num2: number = 0;
+    result:number=0;
+    calculate(K:Event){
+      let btn =  K.target as HTMLInputElement;
+      if(btn.value=="add"){
+        this.result = this.num1 + this.num2;
+      }
+      else if(btn.value=="subtract"){
+        this.result = this.num1 - this.num2;
+        }
+      else if(btn.value == "mul"){
+        this.result = this.num1 * this.num2;
+      }
+      else{
+        this.result = this.num1 / this.num2;
+      }
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,26 @@ import { ThirdComponent } from './third/third.component';
 import { HeaderComponent } from './page/header/header.component';
 import { FooterComponent } from './page/footer/footer.component';
 import { BodyComponent } from './page/body/body.component';
-
+import { AddNumbersComponent } from './add-numbers/add-numbers.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,BodyComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,BodyComponent,AddNumbersComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  i=100;
+  a:string="show";
+  k=false;
+  show(){
+    this.k= !this.k;
+    if(this.k===true){
+      this.a="hide";
+    }
+    else{
+      this.a="show";
+    }
+  }
   title = 'project';
 }
